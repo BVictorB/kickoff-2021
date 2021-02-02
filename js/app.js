68f210b4d0fa6660161f6bda0a9a5bd3a6f7a311
bvictorb <victory-boucher@hotmail.com>
2021-02-02 16:03:34 +0100

    added alot of fun stuff :^)

--- js/app.js
+++ js/app.js
@@ -1,18 +1,59 @@
-const url = 'https://600ff44f6c21e1001704fac2.mockapi.io/minor-web/api/squads/1/teams/1/members'
+const 
+  url = 'https://600ff44f6c21e1001704fac2.mockapi.io/minor-web/api/squads/1/teams/1/members',
+  container = document.querySelector('main')
 
 fetch(url)
   .then(
     (res) => {
-      if (res.status !== 200) {
-        console.log(`Error: status code ${res.status}`)
-        return
-      }
-
       res.json().then((data) => {
-        console.log(data)
+        createNameElements(data)
+        createCardAnimations()
       })
     }
   )
   .catch((err) => {
     console.log(`Error: ${err}`)
   })
+
+const createNameElements = (data) => {
+  data.forEach(data => {
+    const anchor = document.createElement('a')
+    anchor.href = data.url
+    anchor.target = '_blank'
+
+    const person = document.createElement('div')
+    person.classList.add('card')
+
+    const image = document.createElement('img')
+    image.src = data.mugshot
+    person.appendChild(image)
+
+    const name = document.createElement('h2')
+    name.innerHTML = data.name
+    person.appendChild(name)
+
+    anchor.appendChild(person)
+    container.appendChild(anchor)
+  })
+}
+
+const createCardAnimations = () => {
+  const cards = document.querySelectorAll('.card')
+
+  const transformCard = (e) => {
+    const 
+      height = e.target.clientHeight,
+      width = e.target.clientWidth,
+      rotationY = 20 * ((e.layerX - width / 2) / width),
+      rotationX = -20 * ((e.layerY - height / 2) / height)
+
+    e.target.style.transform = `perspective(1000px) scale(1.1) rotateX(${rotationX}deg) rotateY(${rotationY}deg)`
+  }
+
+  cards.forEach(card => {
+    card.addEventListener('mousemove', transformCard)
+    card.addEventListener('mouseout', () => {
+      card.style.transform = 'perspective(1000px) scale(1) rotateX(0) rotateY(0)'
+    })
+  })
+}
